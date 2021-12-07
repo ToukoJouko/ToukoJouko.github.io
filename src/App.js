2ad240123a6fedc2207ceecdb51b353035468d66
Touko Pakarinen <touko@meili.fi>
2021-12-07 12:38:24 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./components/componentStyles.css";
 import NavBar from "./components/NavBar";
+import Intro from "./components/Intro";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Intro />
     </div>
   );
 };
---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Intro = (props) => {
+  return (
+    <div className="intro">
+      <div className="greeting">Hello</div>
+      <div className="name">TOUKO PAKARINEN</div>
+      <div>
+        <div className="desc">professional kela rotta</div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;

c2e72662507e3e775ffd82a2ba536b334c2ff9a1
Touko Pakarinen <touko@meili.fi>
2022-01-11 18:15:36 +0200

    dropdown changed

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { useState } from "react";
-import { CSSTransition } from "react-transition-group";
 
 const DropDown = (props) => {
   const [open, setOpen] = useState(false);
-  const [slideAnimation, setSlideAnimation] = useState(false);
   const closedText = "SHOW INFO";
   const openText = "HIDE INFO";
 
   const handleClick = () => {
     setOpen(!open);
-    setSlideAnimation(!slideAnimation);
   };
 
   return (
@@ -18,14 +15,13 @@ const DropDown = (props) => {
       <div className="dropdown_button" onClick={handleClick}>
         {open ? openText : closedText}
       </div>
-      <CSSTransition
-        in={slideAnimation}
-        timeout={300}
-        classNames="slide"
-        unmountOnExit
-      >
-        <div>{open && props.children}</div>
-      </CSSTransition>
+      {open ? (
+        <div className="slide">
+          <div>{open && props.children}</div>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
